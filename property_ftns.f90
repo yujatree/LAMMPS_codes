9eb283399fa0994497c06857f1d9f6d016fb510a
Jina <sgn07230@naver.com>
2025-06-25 17:08:28 +0900

    update

--- property_ftns.f90
+++ property_ftns.f90
@@ -1,6 +1,20 @@
 ! ----------------------------------------------------------------------------------------------	
 module property_ftns
 
+   !------------------------------------------------!
+   !                                                !
+   !              LAMMPS ANALYSIS PROGRAM           !
+   !                   made by Jina                 !
+   !						    !
+   !          	       DCD  : 60 		    !
+   !          	       MSD  : 225                   !
+   !          	       NGP  : 332    	            !
+   !          	       ICD  : 467                   !
+   !          	       RDF  : 569		    !
+   !		       GSRT :			    !
+   !						    !
+   !------------------------------------------------!
+
    ! Variables Declaration----------------------------------------------------------------------	
 	
    implicit none
@@ -30,6 +44,7 @@ module property_ftns
    type(traj_ptr), allocatable, dimension(:) :: x, y, z
    type(box_ptr), allocatable, dimension(:)  :: box, volume
    integer                                   :: nmedia, total_traj, atom_idx
+   integer, parameter			     :: crst_nmedia=4032
    integer, allocatable, dimension(:)        :: ntraj
 					       !num_of_args : number of ensembles
    				               !nmedia : number of particles
@@ -42,7 +57,7 @@ module property_ftns
 
 ! ----------------------------------------------------------------------------------------------	
 
-   subroutine dcd_reader() ! for ensembles
+   subroutine dcd_reader ! for ensembles
 
    !------------------------------------------------!
    !                                                !
@@ -162,9 +177,16 @@ module property_ftns
 		    volume(i)%v(total_traj))
 	enddo
 
-	num_Li=nmedia*3/8
-	num_P=nmedia/8
-	num_S=nmedia/2
+	if (mod(nmedia,8)==0) then
+	   num_Li=nmedia*3/8
+	   num_P=nmedia/8
+	   num_S=nmedia/2
+	else
+	   print*, "There may be some defects in the system"
+	   num_Li=crst_nmedia*3/8-(crst_nmedia-nmedia)/6
+	   num_P=crst_nmedia/8-(crst_nmedia-nmedia)/6
+	   num_S=crst_nmedia/2-(crst_nmedia-nmedia)*2/3
+	endif
 
 	print*
 	print*, "---------LPS System----------"
@@ -200,7 +222,7 @@ module property_ftns
 
 ! ----------------------------------------------------------------------------------------------	
 
-   subroutine mean_squared_displacement()
+   subroutine mean_squared_displacement
 
 	! Variables Declaration-----------------------------------------------------------------
         
@@ -230,7 +252,7 @@ module property_ftns
 
 	!---------------------------------------------------------------------------------------
 	
-	do i=1, int(total_traj*0.7)
+	do i=1, int(dble(total_traj*0.7d0))
 	  
 	   if (mod(i,10)==0) write(*,998) " Processing ", i, " th trajectory"
 
@@ -309,7 +331,7 @@ module property_ftns
 
 ! ----------------------------------------------------------------------------------------------	
 
-   subroutine non_gaussian_parameter()
+   subroutine non_gaussian_parameter
 
 	! Variables Declaration-----------------------------------------------------------------
         
@@ -442,19 +464,21 @@ module property_ftns
 
 ! ----------------------------------------------------------------------------------------------
 	
-   subroutine ionic_conductivity()
+   subroutine ionic_conductivity
 
 	! Variables Declaration-----------------------------------------------------------------
         
         implicit none
  
         logical 		       :: file_exists
+        character(20)		       :: input_line
 
         integer 		       :: ios, temp, last_step
-	double precision	       :: msd, std, icd_avg, icd_std, vol, var, &
+	double precision	       :: msd, std, vol, const, diff_coeff, icd_avg, icd_std, &
 					  last_msd, last_std		   ! Only for Li+ ions
-	double precision, parameter    :: const=1.6d0*1.6d0/1.38d2/6d0     ! Constant of N-E eq.
-									   ! Optimized w/ 'ps'
+	double precision, parameter    :: q=1.60217663d-19, &
+					  kb=1.380649d-23,&   ! Constant of N-E eq.
+					  A_to_cm=1.d-8, ps_to_s=1.d-12	   ! Optimized w/ 'ps'
 
 	! Setting-------------------------------------------------------------------------------
 
@@ -473,11 +497,11 @@ module property_ftns
 	inquire(file="MSD.out", exist=file_exists)
 
         if (file_exists) then
-	   call dcd_reader() ! for volume
+	   call dcd_reader ! for volume
 	else
 	   print*, "There is no 'MSD.out' file."
 	   print*, "Calling MSD program..."
-	   call mean_squared_displacement()
+	   call mean_squared_displacement
 	endif
 
 	! Input file
@@ -488,35 +512,46 @@ module property_ftns
 	! --------------------------------------------------------------------------------------
 	print*, "Default = 300 K"
 	write(*,'(A)',advance='no') " Enter the temperature : "
-	read(*,'(A)',iostat=ios) temp
+	read(*,'(A)',iostat=ios) input_line
 
-	if (ios .ne. 0) temp=300
+	if (trim(input_line)=="") then
+	   temp=300
+	else
+	   read(input_line, *, iostat=ios) temp
+           if (ios /= 0) then
+              print*, "Invalid input. Using default temperature."
+              temp = 300
+           endif
+	endif
 
-	do 
- 	   read(101,997,iostat=ios) last_step, msd, std
-	   if (ios .ne. 0) exit
+	print*, temp
+	read(101,*)
+	do
+	   last_step=i ; last_msd=msd ; last_std=std 
+ 	   read(101,997) i, msd, std
+	   if (i == 0) exit
 	enddo
-
-	print*, last_step
-	print*, last_msd
-	print*, last_std
 
 	! Volume ensemble average
 	vol=0.d0
 	do j=1, num_of_args
 	   vol=vol+volume(j)%v(last_step) ! for last step
 	enddo
-	vol=vol/dble(num_of_args)
+	vol=vol/dble(num_of_args)*A_to_cm*A_to_cm*A_to_cm
 
-	print*, vol	
-	var=const*dble(num_Li)/dble(temp)/vol/dble(last_step)
-	print*, var
-	icd_avg=var*last_msd ! S/cm
-	icd_std=var*last_std
-	
+	const=(dble(num_Li)/vol)*q*q/kb/dble(temp)     ! (C^2/J.cm^3)
+	diff_coeff=last_msd/6d0/dble(last_step)        ! (A^2/ps)
+	diff_coeff=diff_coeff*A_to_cm*A_to_cm/ps_to_s  ! (cm^2/s)
+	icd_avg=const*diff_coeff		       ! (A/Jcm=S/cm)
+	icd_std=const*diff_coeff/last_msd*last_std
+	print*, const	
 	write(103,'(A)') "# Ionic Conductivity     Standard deviation"
 	write(103,*) icd_avg, icd_std
-
+	
+	print*, "Diffusion Coefficient : "
+	print*, diff_coeff, "(cm2/s)"
+	print*, "Ionic conductivity : "
+	print*, icd_avg, "(S/m)"
 	print*
 	print*, "   Saved as 'ICD.out' file   "
         print*, "-----------------------------"
@@ -531,33 +566,28 @@ module property_ftns
 
 ! ----------------------------------------------------------------------------------------------	
 
-   subroutine radial_distribution_function()
+   subroutine radial_distribution_function
 
 	! Variables Declaration-----------------------------------------------------------------
         
         implicit none 
-        integer :: bin
-        double precision :: dx, dy, dz, distance, bin_size, median, r
+        integer :: bin, bin_num, idx1, idx2
+        double precision :: dx, dy, dz, dist, bin_size, r
 	double precision, parameter :: pi=datan(1.d0)*4.d0
 	double precision, dimension(7) :: rdf_avg, rdf_std
-	double precision, dimension(:,:), allocatable :: g, rdf, square_rdf
+	double precision, dimension(:), allocatable   :: median
+	double precision, dimension(:,:), allocatable :: g, g_avg, rdf, square_rdf
 
 	! Setting-------------------------------------------------------------------------------
 
-	call dcd_reader()
+	call dcd_reader
 
-	open(103, file='RDF.out', status='unknown')
+	open(104, file='RDF.out', status='unknown')
 
-	write(103,*) "# r(A) Li_Li P_P S_S Li_P Li_S P_S Total"	
-999	format(F10.3,F30.16,F30.16,F30.16,F30.16,F30.16,F30.16,F30.16,F30.16,&
-		     F30.16,F30.16,F30.16,F30.16,F30.16,F30.16/)
-
-	allocate(g(7,bin), rdf(7,bin), square_rdf(7,bin)) ! 수정 필요!!!!!!!!!!!!!!!!!!!
-
-	! Default values
-	bin_size=0.05d0
-	bin=1000
-
+	write(104,*) "# r(A) Li_Li P_P S_S Li_P Li_S P_S Total"	
+999	format(F10.3,14(F30.16)/)
+998	format(A,I4,A)
+	
 	print*, "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
 	print*, ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 	print*
@@ -567,16 +597,206 @@ module property_ftns
 	print*
 	print*, "Enter the size/number of bins"
 	print*, "        (Default : 0.05/1000)"
-	read(*,*) bin_size, bin
-	print*, bin_size, bin
 	
+	! Default values
+	bin_size=0.05d0
+	bin=1000
+	
+	allocate(median(bin))
+	allocate(g(7,bin), g_avg(7,bin), rdf(7,bin), square_rdf(7,bin))
+
 	!---------------------------------------------------------------------------------------
 	
-	!do i=1, total_traj
-	
-	 
+	rdf=0.d0 ; square_rdf=0.d0
+
+	do i=1, num_of_args
 	   
+	   write(*,998) " Processing ", i, " th ensemble"
+
+	   g_avg=0.d0
+
+	   do j=1, total_traj
+
+	      if (mod(j,10)==0) write(*,998) " Processing ", j, " th trajectory"
+
+	      g=0.d0
 	
+	      ! Li-Li RDF
+	      do k=1, num_Li
+		 do l=1, k-1
+
+		    if (k==1) goto 1001
+		    dx=x(i)%p(j,k)-x(i)%p(j,l)
+		    dy=y(i)%p(j,k)-y(i)%p(j,l)
+		    dz=z(i)%p(j,k)-z(i)%p(j,l)
+		    dx=dx-nint(dx/box(i)%p(j,1))*box(i)%p(j,1)
+		    dy=dy-nint(dy/box(i)%p(j,3))*box(i)%p(j,3)
+		    dz=dz-nint(dz/box(i)%p(j,6))*box(i)%p(j,6)
+		    dist=sqrt(dx*dx+dy*dy+dz*dz)
+
+		    bin_num=int(dist/bin_size)+1
+		    g(1,bin_num)=g(1,bin_num)+2.d0
+
+		 enddo
+1001	      enddo
+	     
+	      ! P-P RDF
+	      do k=1, num_P
+		 idx1=num_Li+k    ! To match P idx
+
+		 do l=1, k-1
+
+		    if (k==1) goto 1002
+		    idx2=num_Li+l ! To match P idx
+		    dx=x(i)%p(j,idx1)-x(i)%p(j,idx2)
+		    dy=y(i)%p(j,idx1)-y(i)%p(j,idx2)
+		    dz=z(i)%p(j,idx1)-z(i)%p(j,idx2)
+		    dx=dx-nint(dx/box(i)%p(j,1))*box(i)%p(j,1)
+		    dy=dy-nint(dy/box(i)%p(j,3))*box(i)%p(j,3)
+		    dz=dz-nint(dz/box(i)%p(j,6))*box(i)%p(j,6)
+		    dist=sqrt(dx*dx+dy*dy+dz*dz)
+
+		    bin_num=int(dist/bin_size)+1
+		    g(2,bin_num)=g(2,bin_num)+2.d0
+
+		 enddo
+1002	      enddo
+
+	
+	      ! S-S RDF
+	      do k=1, num_S
+		 idx1=num_Li+num_P+k    ! To match S idx
+		 do l=1, k-1
+
+		    if (k==1) goto 1003
+		    idx2=num_Li+num_P+l ! To match S idx
+		    dx=x(i)%p(j,idx1)-x(i)%p(j,idx2)
+		    dy=y(i)%p(j,idx1)-y(i)%p(j,idx2)
+		    dz=z(i)%p(j,idx1)-z(i)%p(j,idx2)
+		    dx=dx-nint(dx/box(i)%p(j,1))*box(i)%p(j,1)
+		    dy=dy-nint(dy/box(i)%p(j,3))*box(i)%p(j,3)
+		    dz=dz-nint(dz/box(i)%p(j,6))*box(i)%p(j,6)
+		    dist=sqrt(dx*dx+dy*dy+dz*dz)
+
+		    bin_num=int(dist/bin_size)+1
+		    g(3,bin_num)=g(3,bin_num)+2.d0
+
+		 enddo
+1003	      enddo
+
+	      ! Li-P RDF
+	      do k=1, num_Li
+		 do l=1, num_P
+
+		    idx1=num_Li+l ! To match P idx
+		    dx=x(i)%p(j,k)-x(i)%p(j,idx1)
+		    dy=y(i)%p(j,k)-y(i)%p(j,idx1)
+		    dz=z(i)%p(j,k)-z(i)%p(j,idx1)
+		    dx=dx-nint(dx/box(i)%p(j,1))*box(i)%p(j,1)
+		    dy=dy-nint(dy/box(i)%p(j,3))*box(i)%p(j,3)
+		    dz=dz-nint(dz/box(i)%p(j,6))*box(i)%p(j,6)
+		    dist=sqrt(dx*dx+dy*dy+dz*dz)
+
+		    bin_num=int(dist/bin_size)+1
+		    g(4,bin_num)=g(4,bin_num)+1.d0
+
+		 enddo
+	      enddo
+
+	      ! Li-S RDF
+	      do k=1, num_Li
+		 do l=1, num_S
+
+		    idx1=num_Li+num_P+l ! To match S idx
+		    dx=x(i)%p(j,k)-x(i)%p(j,idx1)
+		    dy=y(i)%p(j,k)-y(i)%p(j,idx1)
+		    dz=z(i)%p(j,k)-z(i)%p(j,idx1)
+		    dx=dx-nint(dx/box(i)%p(j,1))*box(i)%p(j,1)
+		    dy=dy-nint(dy/box(i)%p(j,3))*box(i)%p(j,3)
+		    dz=dz-nint(dz/box(i)%p(j,6))*box(i)%p(j,6)
+		    dist=sqrt(dx*dx+dy*dy+dz*dz)
+
+		    bin_num=int(dist/bin_size)+1
+		    g(5,bin_num)=g(5,bin_num)+1.d0
+
+		 enddo
+	      enddo
+
+	
+	      ! P-S RDF
+	      do k=1, num_P
+		 idx1=num_Li+k
+		 do l=1, num_S
+
+		    idx2=num_Li+num_P+l ! To match P idx
+		    dx=x(i)%p(j,idx1)-x(i)%p(j,idx2)
+		    dy=y(i)%p(j,idx1)-y(i)%p(j,idx2)
+		    dz=z(i)%p(j,idx1)-z(i)%p(j,idx2)
+		    dx=dx-nint(dx/box(i)%p(j,1))*box(i)%p(j,1)
+		    dy=dy-nint(dy/box(i)%p(j,3))*box(i)%p(j,3)
+		    dz=dz-nint(dz/box(i)%p(j,6))*box(i)%p(j,6)
+		    dist=sqrt(dx*dx+dy*dy+dz*dz)
+
+		    bin_num=int(dist/bin_size)+1
+		    g(6,bin_num)=g(6,bin_num)+1.d0
+
+		 enddo
+	      enddo
+
+	      do k=1, bin
+	         g(1,k)=g(1,k)/dble(num_Li*num_Li)
+	         g(2,k)=g(2,k)/dble(num_P*num_P)
+	         g(3,k)=g(3,k)/dble(num_S*num_S)
+	         g(4,k)=g(4,k)/dble(num_Li*num_P)
+	         g(5,k)=g(5,k)/dble(num_Li*num_S)
+	         g(6,k)=g(6,k)/dble(num_P*num_S)
+		 g(7,k) = ( &				! TOTAL RDF
+		    dble(num_Li*num_Li)*g(1,k) + &
+		    dble(num_P*num_P)*g(2,k) + &
+		    dble(num_S*num_S)*g(3,k) + &
+		    dble(num_Li*num_P)*g(4,k) + &
+		    dble(num_Li*num_S)*g(5,k) + &
+ 		   dble(num_P*num_S)*g(6,k) ) / dble(nmedia*nmedia)
+	      enddo
+
+	      g=g*volume(i)%v(j) ! division by number of atoms & number density = x volume
+
+	      do k=1, bin
+	         do l=1, 7
+		     g_avg(l,k)=g_avg(l,k)+g(l,k)
+		 enddo
+	      enddo
+
+	   enddo
+
+	   do j=1, bin
+	      median(j)=dble(j-0.5d0)*bin_size
+	      r=dble(j-1)*bin_size
+	
+	      do k=1,7
+	 	 if (j==1) then
+		    g_avg(k,j)=0.d0
+		 else
+		    g_avg(k,j)=g_avg(k,j)/dble(total_traj)/(4.d0*pi*r*r*bin_size)
+		 endif
+	         rdf(k,j)=rdf(k,j)+g_avg(k,j)
+		 square_rdf(k,j)=square_rdf(k,j)+g_avg(k,j)*g_avg(k,j)
+	      enddo
+
+	   enddo
+
+	enddo
+
+	do i=1, bin
+	   do j=1,7
+	      rdf_avg(j)=rdf(j,i)/dble(num_of_args)
+	      rdf_std(j)=square_rdf(j,i)/dble(num_of_args)-rdf_avg(j)*rdf_avg(j)
+	   enddo
+	   write(104,999,advance='no') median(i), rdf_avg(1), rdf_std(1), rdf_avg(2), rdf_std(2),&
+	      			      rdf_avg(3), rdf_std(3), rdf_avg(4), rdf_std(4), rdf_avg(5),&
+	      			      rdf_std(5), rdf_avg(6), rdf_std(6), rdf_avg(7), rdf_std(7)
+	enddo
+		 	
 	!---------------------------------------------------------------------------------------
 	print*
 	print*, "   Saved as 'RDF.out' file   "
@@ -584,9 +804,101 @@ module property_ftns
 	print*, "(   COMPLETE!   )>    V(0_0)V"
         print*, "-----------------------------"
 	print*
-	close(103)
+
+	deallocate(g,rdf,square_rdf)
+	close(104)
 
    end subroutine radial_distribution_function
 
 ! ----------------------------------------------------------------------------------------------	
 end module property_ftns
+! ----------------------------------------------------------------------------------------------
+
+program main
+
+   use property_ftns
+
+   implicit none
+   integer :: input
+   character(1) :: answer
+
+123 format(A/)
+   print*
+   write(*,123,advance='no') "======================================================================================================"
+   write(*,123,advance='no') "   __    ___    __  ___ __  ___ ___   ____    ___   _____ ___      ____ ____ __    ____ ____          " 
+   write(*,123,advance='no') "  / /   / _ |  /  |/  //  |/  // _ \ / __/   / _ \ / ___// _ \    / __//  _// /   / __// __/          " 
+   write(*,123,advance='no') " / /__ / __ | / /|_/ // /|_/ // ___/_\ \    / // // /__ / // /   / _/ _/ / / /__ / _/ _\ \            " 
+   write(*,123,advance='no') "/____//_/ |_|/_/  /_//_/  /_//_/   /___/   /____/ \___//____/   /_/  /___//____//___//___/            " 
+   write(*,123,advance='no') "      ___   ___   ____   ___   ____ ___  ________  __    ___   ___   ____   _____ ___   ___    __  ___"
+   write(*,123,advance='no') "     / _ \ / _ \ / __ \ / _ \ / __// _ \/_  __/\ \/ /   / _ \ / _ \ / __ \ / ___// _ \ / _ |  /  |/  /"
+   write(*,123,advance='no') "    / ___// , _// /_/ // ___// _/ / , _/ / /    \  /   / ___// , _// /_/ // (_ // , _// __ | / /|_/ / " 
+   write(*,123,advance='no') "   /_/   /_/|_| \____//_/   /___//_/|_| /_/     /_/   /_/   /_/|_| \____/ \___//_/|_|/_/ |_|/_/  /_/  " 
+   write(*,123,advance='no') " "
+1  write(*,123,advance='no') "======================================================================================================"
+                                                                                             
+   print*
+   print*, "          [1] Mean Squared Displacement"
+   print*, "          [2] Non-Gaussian Parameter"
+   print*, "          [3] Ionic Conductivity"
+   print*, "          [4] Radial Distribution Function"
+   print*, "          [5] Self-part van Hove Correlation Function"
+   print*, "          [6] Distinct-part van Have Correlation Function"
+   print*, "          [7] Hop Function"
+   print*, "          [8] EXIT"
+   print*
+
+   write(*,'(A)',advance='no') "           [Enter the property index] : "
+!   read(*,*) input
+   input=4
+   print*
+   write(*,123,advance='no') "======================================================================================================"
+   print*
+
+   if (input==1) then
+      call mean_squared_displacement
+   elseif (input==2) then
+      call non_gaussian_parameter
+   elseif (input==3) then
+      call ionic_conductivity
+   elseif (input==4) then
+      call radial_distribution_function
+   elseif (input==5) then
+      print*, "   Sorry, It's Updating... :( "
+      print*
+      goto 1
+      !call self_part_van_hove_correlation_function()
+   elseif (input==6) then
+      print*, "   Sorry, It's Updating... :( "
+      print*
+      goto 1
+      !call distinct_part_van_hove_correlation_function()
+   elseif (input==7) then
+      print*, "   Sorry, It's Updating... :( "
+      print*
+      goto 1
+   elseif (input==8) then
+      print*, "   PROGRAM ENDS ..."
+      print*
+      stop
+   else
+      print*, "   Enter the right index"
+      goto 1
+   end if
+  
+   print*
+   write(*,123,advance='no') "======================================================================================================"
+   print*
+   write(*,'(A)',advance='no') "    Do you want to get another property? (Y/N) : "
+  ! read(*,'(A)') answer
+answer='n'
+   if ((answer=='y') .or. (answer=='Y')) goto 1
+  
+   print*
+   print*, "   PROGRAM ENDS ..."
+   print*
+   write(*,123,advance='no') "======================================================================================================"
+   print*
+
+end program
+
+! ----------------------------------------------------------------------------------------------	
